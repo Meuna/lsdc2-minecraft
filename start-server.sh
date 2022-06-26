0a43d04886624f2b2504892f6bb9c870f685152e
Thomas Bany <mun.public@gmail.com>
2022-06-26 22:52:19 +0200

    Works locally

---
+++ start-server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export HOME=/tmp
+
+shutdown() {
+    kill -INT $pid
+}
+
+if [ "$VERSION" = "latest" ]; then
+    VERSION=$(curl -s $VERSION_MANIFEST | jq --raw-output '.latest.release')
+fi
+RELEASE_MANIFEST=$(curl -s $VERSION_MANIFEST | jq --raw-output --arg VER $VERSION '[.versions[] | select(.id == $VER)][0].url')
+SERVER_URL=$(curl -s $RELEASE_MANIFEST | jq --raw-output '.downloads.server.url')
+curl -s $SERVER_URL -o minecraft-server.jar
+
+trap shutdown SIGINT SIGTERM
+
+java -Xmx4G -jar minecraft-server.jar --port $SERVER_PORT --nogui --world $WORLD_NAME &
+pid=$!
+wait $pid
