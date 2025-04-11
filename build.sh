a1346acd297c7df4114903dde4c488bfc34f47db
Thomas Bany <mun.bany@gmail.com>
2025-04-11 21:04:07 +0200

    Build script can now be run from any wd

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-podman build . -t docker.io/meuna/lsdc2:minecraft \
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+podman build $script_dir -t docker.io/meuna/lsdc2:minecraft \
 && podman push docker.io/meuna/lsdc2:minecraft
